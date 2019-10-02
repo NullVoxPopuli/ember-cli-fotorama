e826987b44ee3f838423ef12991d900422eb4bf6
NullVoxPopuli <LPSego3+dev@gmail.com>
2019-10-02 02:38:40 -0400

    Ran codemods to remove jQuery

--- addon/components/ember-fotorama.js
+++ addon/components/ember-fotorama.js
@@ -4,6 +4,6 @@ import layout from '../templates/components/ember-fotorama';
 export default Ember.Component.extend({
     layout: layout,
     didInsertElement: function() {
-        this.$('.fotorama-cotainer').fotorama();
+        this.element.querySelectorAll('.fotorama-cotainer').forEach(el => el.addEventListener());
     }
 });
